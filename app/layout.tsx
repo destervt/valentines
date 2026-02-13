1fd00f38a3167ad059f46993a261609cf239bd4e
выпустили погулять . <68183599+destervt@users.noreply.github.com>
2026-02-13 15:23:51 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Will You Be My Valentine?",
+  title: "Будешь моей любимой?",
 };
 
 export default function RootLayout({
